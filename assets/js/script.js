db6b2b187992e26e02739a2b0c25049b9fbee27b
Arta Kusuma <arta@mindimedia.com>
2023-12-03 21:01:09 +0800

    First Commit

---
+++ assets/js/script.js
@@ -0,0 +1,77 @@
+var swiper = new Swiper(".mySwiper", {
+  mousewheel: true,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },  
+  breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2.5,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 2.5,
+          spaceBetween: 20,
+        },
+    },
+});
+
+var swiper = new Swiper(".mySwiperPriority", {
+  mousewheel: true,
+  pagination: {
+    el: ".swiper-pagination2",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },  
+  breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2.5,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 2.5,
+          spaceBetween: 20,
+        },
+    },
+});
+
+var swiper = new Swiper(".mySwiper3", {
+  mousewheel: true,
+  pagination: {
+    el: ".swiper-pagination3",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next3",
+    prevEl: ".swiper-button-prev3",
+  },  
+  breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2.5,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 2.5,
+          spaceBetween: 20,
+        },
+    },
+});
